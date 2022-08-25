884bd71e2ae2a71f4918f0769aedfac3459a8894
Dmitriy <kazany.dmitry@gmail.com>
2022-08-25 07:21:57 +0300

    task06 even

---
+++ Task06_Even/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.Read());
+if (number % 2 == 0) 
+{
+Console.WriteLine("Четное");
+}
+else
+{
+Console.WriteLine("Нечетное");
+}
