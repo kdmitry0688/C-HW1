a7b729223effa678608d406dedce5272139fed8c
Dmitriy <kazany.dmitry@gmail.com>
2022-08-25 07:40:58 +0300

    task08 all even till n

---
+++ Task08_AllEven/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите границу диапазона: ");
+int n = Int32.Parse(Console.ReadLine());
+for (int x = 2; x < n; x = x + 1)       
+if (x % 2 == 0)            
+Console.WriteLine(x);
